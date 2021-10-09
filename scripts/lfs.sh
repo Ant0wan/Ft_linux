441aed7a805c91f900941ff0c4184cbc78a7e5f1
Antoine Barthelemy <antoine@linux.com>
2021-10-09 18:27:02 +0200

    feat(scripts): adding lfs script and partitioning section

--- scripts/lfs.sh
+++ scripts/lfs.sh
@@ -6,7 +6,11 @@ export LFS_DISK=/dev/sda
 
 if ! grep -q "^${LFS}" /proc/mounts; then
 	parted ${LFS_DISK} mklabel gpt
+
 	parted ${LFS_DISK} mkpart primary ext4 1 100MB
 	mkfs.ext4 ${LFS_DISK}1
 	parted ${LFS_DISK} set 1 boot on
+
+	parted ${LFS_DISK} mkpart primary ext4 100MB 50GB
+	mkfs.ext4 ${LFS_DISK}2
 fi
