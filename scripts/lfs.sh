e44d7095edef8d80e9a67ba9f19c3cc96206d16a
Antoine Barthelemy <antoine@linux.com>
2021-10-10 15:34:17 +0200

    feat(scripts): packages list

--- scripts/lfs.sh
+++ scripts/lfs.sh
@@ -5,7 +5,7 @@ export LFS=/mnt/lfs
 export LFS_TGT=x86_64-lfs-linux-gnu
 export LFS_DISK=/dev/sda
 
-if ! grep -q "^${LFS}" /proc/mounts; then
+if ! grep -q "${LFS}" /proc/mounts; then
 	parted ${LFS_DISK} mklabel gpt
 
 	parted ${LFS_DISK} mkpart primary ext4 1 100MB
@@ -18,8 +18,8 @@ if ! grep -q "^${LFS}" /proc/mounts; then
 	mount ${LFS_DISK}2 $LFS
 fi
 
-mkdir -pv ${LFS}/{sources,tools,boot,etc,bin,lib,sbin,usr,var}
+mkdir -vp ${LFS}/{sources,tools,boot,etc,bin,lib,sbin,usr,var}
 chown -v lfs ${LFS}/{sources,tools,boot,etc,bin,lib,sbin,usr,var}
 case $(uname -m) in
-	x86_64) mkdir -pv lfs ${LFS}/lib64 && chown -v lfs ${LFS}/lib64 ;;
+	x86_64) mkdir -pv ${LFS}/lib64 && chown -v lfs ${LFS}/lib64 ;;
 esac
